bd91cd674ad6262b86fd70165a2fd2afc637503f
rPlakama <rPlakama@proton.me>
2025-11-06 23:11:24 -0300

    Mais CSS.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import "./Footer.css";
 
 const Footer = () => {
-  return (
-    <footer className="footer">
-      <p>Feito com React e a API do GitHub</p>
-    </footer>
-  );
+	return (
+		<footer className="footer">
+			<p>Feito com React e a API do GitHub</p>
+		</footer>
+	);
 };
 
 export default Footer;
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,45 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import List from './List';
+import "./Section.css";
 
 const Section = () => {
-  const [repos, setRepos] = useState([]);
-  const [language, setLanguage] = useState('Todos');
+	const [repos, setRepos] = useState([]);
+	const [language, setLanguage] = useState('Todos');
 
-  const languages = ['Todos', 'JavaScript', 'Python', 'Go', 'Ruby', 'Java', 'Nix'];
+	const languages = ['Todos', 'JavaScript', 'Python', 'Go', 'Ruby', 'Java', 'Nix'];
 
-  useEffect(() => {
-    const fetchRepos = async () => {
-      let url = 'https://api.github.com/search/repositories?q=stars:>=10000&sort=stars&order=desc&per_page=28';
-      if (language !== 'Todos') {
-        url = `https://api.github.com/search/repositories?q=stars:>=10000+language:${language}&sort=stars&order=desc&per_page=28`;
-      }
-      if (language == 'Nix') {
-	url = `https://api.github.com/search/repositories?q=stars:>=1000+language:${language}&sort=stars&order=desc&per_page=28`;
-      }
-      try {
-        const response = await axios.get(url);
-        setRepos(response.data.items);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
+	useEffect(() => {
+		const fetchRepos = async () => {
+			let url = 'https://api.github.com/search/repositories?q=stars:>=10000&sort=stars&order=desc&per_page=28';
+			if (language !== 'Todos') {
+				url = `https://api.github.com/search/repositories?q=stars:>=10000+language:${language}&sort=stars&order=desc&per_page=28`;
+			}
+			if (language == 'Nix') {
+				url = `https://api.github.com/search/repositories?q=stars:>=1000+language:${language}&sort=stars&order=desc&per_page=28`;
+			}
+			try {
+				const response = await axios.get(url);
+				setRepos(response.data.items);
+			} catch (error) {
+				console.error('Error fetching data:', error);
+			}
+		};
 
-    fetchRepos();
-  }, [language]);
+		fetchRepos();
+	}, [language]);
 
-  return (
-    <section>
-      <div>
-        {languages.map(lang => (
-          <button key={lang} onClick={() => setLanguage(lang)}>
-            {lang}
-          </button>
-        ))}
-      </div>
-      <List repos={repos} />
-    </section>
-  );
+	return (
+		<section>
+			<div>
+				{languages.map(lang => (
+					<button key={lang} onClick={() => setLanguage(lang)}
+				  className={language === lang ? 'active-button' : 'inactive-button'}>
+						{lang}
+					</button>
+				))}
+			</div>
+			<List repos={repos} />
+		</section>
+	);
 };
 
 export default Section;
